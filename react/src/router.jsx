0957bbf144f018cfd3d5a34496ac93858ff508f2
Kall <samueluche432@gmail.com>
2024-04-22 14:26:21 +0100

    modified:   src/containers/index.js 	deleted:    src/containers/particle-js/ParticleBackground.jsx 	deleted:    src/containers/particle-js/app.js 	deleted:    src/containers/particle-js/particles.js 	deleted:    src/main copy.jsx 	modified:   src/router.jsx

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -2,15 +2,14 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import DefaultLayout from "./layout/defaultlayout/DefaultLayout";
 import GuestLayout from "./layout/guestlayout/GuestLayout";
 import Board from "./containers/board/Board";
-import Board from "./containers/signup/Signup";
+import Signup from "./containers/signup/Signup";
 import Login from "./containers/login/Login";
 import NotFound from "./display/notfound/NotFound";
-import NotFound from "./display/result/Result";
-import Board from "./Exam";
-import Board from "./App";
-import Board from "./containers/board";
-import Board from "./containers/library/Library";
-import Board from "./containers/whatissepp/Whatsepp";
+import Result from "./display/result/Result";
+import Exam from "./Exam";
+import App from "./App";
+import Library from "./containers/library/Library";
+import Whatsepp from "./containers/whatissepp/Whatsepp";
 
 const router = createBrowserRouter([
     {
