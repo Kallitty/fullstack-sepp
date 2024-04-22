635076a7d3bc947c172b9db84148638b5eb9e601
Kall <samueluche432@gmail.com>
2024-04-22 10:43:53 +0100

    modified:   src/components/sidebar/Sidebar.jsx 	new file:   src/display/index.js 	new file:   src/display/notfound/NotFound.jsx 	new file:   src/display/notfound/notfound.scss 	new file:   src/display/result/Result.jsx 	new file:   src/display/result/result.scss
     Untracked files:
    	../package-lock.json

--- react/src/components/sidebar/Sidebar.jsx
+++ react/src/components/sidebar/Sidebar.jsx
@@ -1,63 +1,68 @@
-import React from 'react'
-import './sidebar.scss'
+import React from "react";
+import "./sidebar.scss";
 import {
-  BiBookAlt,
-  BiHome,
-  BiMessage,
-  BiSolidReport,
-  BiStats,
-  BiFolder,
-  BiTask,
-  BiLogOut,
-} from 'react-icons/bi'
-import { DiAptana } from 'react-icons/di'
+    BiBookAlt,
+    BiHome,
+    BiMessage,
+    BiSolidReport,
+    BiStats,
+    BiFolder,
+    BiTask,
+    BiLogOut,
+} from "react-icons/bi";
+import { DiAptana } from "react-icons/di";
 
 const Sidebar = () => {
-  return (
-    <div className='sepp__sidebar-grand'>
-      <div className='sepp__sidebar-left__menu'>
-        <div className='sepp__sidebar-left-menu--logo'>
-          <BiBookAlt className='sepp__sidebar-left__logo-icon' />
-          <h3>My SEPP</h3>
-        </div>
+    return (
+        <div className="sepp__sidebar-grand">
+            <div className="sepp__sidebar-left__menu">
+                <div className="sepp__sidebar-left-menu--logo">
+                    <BiBookAlt className="sepp__sidebar-left__logo-icon" />
+                    <h3>My SEPP</h3>
+                </div>
 
-        <div className='sepp__sidebar-left-menu--list'>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiHome className='sepp__sidebar-left__icon' />
-            Dashboard
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiSolidReport className='sepp__sidebar-left__icon' />
-            Results
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiFolder className='sepp__sidebar-left__icon' />
-            Report card
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiStats className='sepp__sidebar-left__icon' />
-            Stats
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiMessage className='sepp__sidebar-left__icon' />
-            Message
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <BiTask className='sepp__sidebar-left__icon' />
-            Help
-          </a>
-          <a href='#' className='sepp__sidebar-left__items'>
-            <DiAptana className='sepp__sidebar-left__icon' />
-            Settings
-          </a>
-        </div>
-      </div>
-      <a href='#' className='sepp__sidebar-left__logout'>
-        <BiLogOut className='sepp__sidebar-left__logouticon' />
-        Logout
-      </a>
-    </div>
-  )
-}
+                <div className="sepp__sidebar-left-menu--list">
+                    <Link to="/dashboard" className="sepp__sidebar-left__items">
+                        <BiHome className="sepp__sidebar-left__icon" />
+                        Dashboard
+                    </Link>
 
-export default Sidebar
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiHome className="sepp__sidebar-left__icon" />
+                        Dashboard
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiSolidReport className="sepp__sidebar-left__icon" />
+                        Results
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiFolder className="sepp__sidebar-left__icon" />
+                        Report card
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiStats className="sepp__sidebar-left__icon" />
+                        Stats
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiMessage className="sepp__sidebar-left__icon" />
+                        Message
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <BiTask className="sepp__sidebar-left__icon" />
+                        Help
+                    </a>
+                    <a href="#" className="sepp__sidebar-left__items">
+                        <DiAptana className="sepp__sidebar-left__icon" />
+                        Settings
+                    </a>
+                </div>
+            </div>
+            <a href="#" className="sepp__sidebar-left__logout">
+                <BiLogOut className="sepp__sidebar-left__logouticon" />
+                Logout
+            </a>
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ react/src/display/result/Result.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./result.scss";
+
+export default function () {
+    return <div>Result</div>;
+}
