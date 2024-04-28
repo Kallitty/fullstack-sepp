7ab301f66b68873efa90478a69d1cbc6d1bf039a
Kall <samueluche432@gmail.com>
2024-04-28 08:38:51 +0100

    Edit  src/layout/defaultlayout/DefaultLayout.jsx

--- react/src/layout/defaultlayout/DefaultLayout.jsx
+++ react/src/layout/defaultlayout/DefaultLayout.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "./defaultlayout.scss";
 import { useEffect } from "react";
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 import axiosClient from "../../axios-client";
 import { Sidebar } from "../../components";
 import { useStateContext } from "../../contexts/ContextProvider";
 
 export default function DefaultLayout() {
     const { user, token, setUser, setToken } = useStateContext();
+    const location = useLocation();
 
     if (!token) {
         return <Navigate to="/app" />;
     }
+
     const onLogout = (e) => {
         e.preventDefault();
-        axiosClient.post("/logout").then(({}) => {
+        axiosClient.post("/logout").then(() => {
             setUser({});
             setToken(null);
         });
@@ -26,20 +28,33 @@ export default function DefaultLayout() {
         });
     }, []);
 
+    const isFullScreenQuiz = location.pathname.startsWith("/exam/"); // Generalized check for any quiz
+
     return (
         <div id="defaultLayout">
-            <aside>
-                <Sidebar />
-            </aside>
-            <div className="content">
-                <header>
-                    <div>Hello {user.name} !</div>
-                    <div>
-                        <a href="#" onClick={onLogout} className="btn-logout">
-                            Logout
-                        </a>
-                    </div>
-                </header>
+            {!isFullScreenQuiz && (
+                <aside>
+                    <Sidebar />
+                </aside>
+            )}
+            <div
+                className="content"
+                style={{ width: isFullScreenQuiz ? "100%" : "auto" }}
+            >
+                {!isFullScreenQuiz && (
+                    <header>
+                        <div>Hello {user.name}!</div>
+                        <div>
+                            <a
+                                href="#"
+                                onClick={onLogout}
+                                className="btn-logout"
+                            >
+                                Logout
+                            </a>
+                        </div>
+                    </header>
+                )}
                 <main>
                     <Outlet />
                 </main>
